76ab8e22f14b0d33cbc2b15772fde2324a322507
Yeongtaek, Hong <mozodev@cckorea.org>
2015-06-05 15:33:48 +0900

    원고 수정 #9 fixed.

--- sites/all/themes/ccgs_logo/templates/page/page.tpl.php
+++ sites/all/themes/ccgs_logo/templates/page/page.tpl.php
@@ -48,11 +48,17 @@ if (ccgs_check_if_no_header() && !ccgs_check_if_admin()) {
 
 <footer id="footer" class="footer" role="footer">
   <div class="container">
+    <a rel="license" class="pull-left media" href="http://creativecommons.org/licenses/by/2.0/kr/">
+      <img alt="크리에이티브 커먼즈 라이선스" style="border-width:0" src="http://i.creativecommons.org/l/by/2.0/kr/88x31.png">
+    </a>
+    <p style="margin-left: 1em; margin-top: 0.5em; display: inline-block">CCKOREA에 의해 작성된 CCKOREA 웹사이트는 크리에이티브 커먼즈 저작자표시 2.0 대한민국 라이선스에 따라 이용할 수 있습니다.</p>
+    <!--
     <?php if ($copyright): ?>
       <small class="copyright pull-left"><?php print $copyright; ?></small>
     <?php endif; ?>
     <?php if (ccgs_check_if_no_header()): ?>
     <small class="pull-right"><a href="#"><?php print t('Back to Top'); ?></a></small>
     <?php endif; ?>
+    -->
   </div>
 </footer>
